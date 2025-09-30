05ebf980e27735434f9ab078883dea620692b841
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-09-30 21:56:40 +0600

    yes

--- src/Tow_D_Array.java
+++ src/Tow_D_Array.java
@@ -1,14 +1,33 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.Arrays;
 public class Tow_D_Array {
     public static void main(String[] args) {
-        int [] num={2,3,4,9,87,1};
-        System.out.println("Here the array length:" + num.length);
-        Arrays.sort(num);  //Arrays number small to big
-        System.out.println(Arrays.toString(num));
-        int [][] num2=new int[2][3];
-        num2[0][0]=1;
-        System.out.println(Arrays.deepToString(num2));
+//        int [] num={2,3,4,9,87,1};
+//        System.out.println("Here the array length:" + num.length);
+//        Arrays.sort(num);  //Arrays number small to big
+//        System.out.println(Arrays.toString(num));
+//        int [][] num2=new int[2][3];
+//        num2[0][0]=1;
+//        System.out.println(Arrays.deepToString(num2));
+//
+        int [][] arr= new int [3][3];
+        int [][] nums= {
+                {1,4,7},
+                {2,5,8},
+                {3,6,9}
+        };
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+
+                System.out.print(nums[i][j] + " ");
+
+            }
+            System.out.println();
+
+        }
+
+
+
     }
 }
 
