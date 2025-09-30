a9dcf342911c2e0f25f589a041b58db1e94b29f1
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-09-30 21:55:21 +0600

    yes

--- src/Array.java
+++ src/Array.java
@@ -1,49 +1,162 @@
+
+//import java.util.Scanner;
+import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
 
-        int [] array= {90,80,70,85,85};
-        System.out.println("Here the index:"+ array[3]);
-        System.out.println("Here the array length: " +array.length);
+//        int [] array= {90,80,70,85,85};
+//        System.out.println("Here the index:"+ array[3]);
+//        System.out.println("Here the array length: " +array.length);
+//
+//        //System.out.println(Arrays.toString(array));
+//
+//        for(int element:array){
+//
+//            System.out.println(element);
+//
+//        }
+//
+//        System.out.println();
+//
+//        for(int i= 0;i<5;i++) {
+//            System.out.println(array[i]);
+//        }
+//
+//        System.out.println("Here the reverse array:");
+//            for (int j = array.length - 1; j>=0; j--) {
+//                System.out.println(array[j]);
+//
+//            }
+//
+//
+//        System.out.println();
+//
+//        String[]student_name={"Zidan","Rin","Ruhani","Zaien"};
+//        System.out.println("Here the index of Student Name: "+student_name[2]);
+//        System.out.println("Here the student length: "+student_name.length);
+//
+//        for(int i= 0;i<4;i++) {
+//            System.out.println(student_name[i]);
+//        }
+//
+//        System.out.print("\n");
+//
+//        System.out.println("Here the reverse array:");
+//        for (int k = student_name.length - 1; k>=0; k--) {
+//            System.out.println(student_name[k]);
+//
+//        }
 
-        //System.out.println(Arrays.toString(array));
+           // array max value find
+//        int [] a={3,4,500,420,30,-20};
+//
+//       int res=Integer.MIN_VALUE;
+//       int min = Integer.MAX_VALUE;
+//
+//       for(int i=0;i<a.length;i++){
+//           if(a[i]>res)
+//           {
+//               res=a[i];
+//           }
+//           if(a[i]<min){
+//               min=a[i];
+//           }
+//       }
+//        System.out.println("MAX Value: " + res );
+//        System.out.println("MIN Value: " + min );
 
-        for(int element:array){
 
-            System.out.println(element);
 
-        }
+//                int[] numbers = {25, 11, 7, 75, 56};
 
-        System.out.println();
+                // Assume first element is both min and max
+//                int max = numbers[0];
+//                int min = numbers[0];
+//
+//                for (int i = 1; i < numbers.length; i++) {
+//                    if (numbers[i] > max) {
+//                        max = numbers[i];  // update max
+//                    }
+//                    if (numbers[i] < min) {
+//                        min = numbers[i];  // update min
+//                    }
+//                }
+//
+//                System.out.println("Maximum value = " + max);
+//                System.out.println("Minimum value = " + min);
 
-        for(int i= 0;i<5;i++) {
-            System.out.println(array[i]);
-        }
+                // sum of array
 
-        System.out.println("Here the reverse array:");
-            for (int j = array.length - 1; j>=0; j--) {
-                System.out.println(array[j]);
+//                 int[]a={100,40,-20,-30};
+//                 int sum= 0;
+//
+//
+//                 for(int i=0;i<a.length;i++){
+//                     sum=sum+a[i];
+//
+//                 }
+//        System.out.println("Array of total sum: "+sum);
+//
+
+
+
+//        int[] a = {100, 40, -20, -30};
+//        int sum = 0;
+//
+//        // Using enhanced for loop
+//        for (int i : a) {
+//            sum += i;
+//            //sum=sum+i;
+//
+//        }
+//
+//        System.out.println("Array total sum: " + sum);
+
+
+
+
+
+                 //array sum max min find
+
+//                Scanner sc = new Scanner(System.in);
+//
+//                // Input array size
+//                System.out.print("Enter the size of array: ");
+//                int n = sc.nextInt();
+//
+//                int[] a = new int[n];
+//
+//                // Input array elements
+//                System.out.println("Enter " + n + " elements:");
+//                for (int i = 0; i < n; i++) {
+//                    a[i] = sc.nextInt();
+//                }
+//
+//                int sum = 0;
+//                int max = a[0];
+//                int min = a[0];
+//
+//                // Processing array
+//                for (int i : a) {
+//                    sum += i;
+//                    if (i > max) max = i;
+//                    if (i < min) min = i;
+//                }
+//
+//                // Output
+//                System.out.println("Array total sum: " + sum);
+//                System.out.println("Maximum value: " + max);
+//                System.out.println("Minimum value: " + min);
+//
+//                sc.close();
+                 int[]a={20,30,45,75,50};
+               //  for(int i=0;i<1;i++){
+                     System.out.println(Arrays.toString(a));
+                 //}
+
 
             }
-
-
-        System.out.println();
-
-        String[]student_name={"Zidan","Rin","Ruhani","Zaien"};
-        System.out.println("Here the index of Student Name: "+student_name[2]);
-        System.out.println("Here the student length: "+student_name.length);
-
-        for(int i= 0;i<4;i++) {
-            System.out.println(student_name[i]);
-        }
-
-        System.out.print("\n");
-
-        System.out.println("Here the reverse array:");
-        for (int k = student_name.length - 1; k>=0; k--) {
-            System.out.println(student_name[k]);
-
         }
 
 
-    }
-}
+
