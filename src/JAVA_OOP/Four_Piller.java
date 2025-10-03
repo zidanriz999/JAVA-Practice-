88c9026853ae0408bd7b313827d003b00b3e430e
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-10-03 10:22:28 +0600

    yes

---
+++ src/JAVA_OOP/Four_Piller.java
@@ -0,0 +1,19 @@
+package JAVA_OOP;
+
+public class Four_Piller {
+    // 1. Encapsulation=Bunding of data and methods into a single unit class
+    // 2. Inheritance=
+    // 3. Polymorphism
+    // 4. Abstroction
+
+    public static void main(String[] args) {
+        System.out.println();
+
+
+
+
+
+    }
+}
+
+
