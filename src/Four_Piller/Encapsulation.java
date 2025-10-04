8a35d3d6a6824e43979d007b7038f0a73b5f94bc
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-10-04 22:27:58 +0600

    yes

--- src/Four_Piller/Encapsulation.java
+++ src/Four_Piller/Encapsulation.java
@@ -1,7 +1,89 @@
 package Four_Piller;
-
 public class Encapsulation {
     public static void main(String[] args) {
+        Car car=new Car();
+        car.color="Blue";
+        //car.speed=50;
+        car.brand="Tata";
+       // car.year=2020;
+        car.model="Safari";
+
+
+
+        car.setSpeed(60);
+
+        car.accelerate(2);
+        System.out.println("Car speed now: "+car.getSpeed());
+        System.out.println("Car Model: "+car.model);
+
 
     }
+
+}
+
+ class Car{
+    String color;
+    String brand;
+   String model;
+   private int speed;
+   private int year;
+
+
+     public void setSpeed() {
+         setSpeed();
+     }
+
+
+
+     public int getYear() {
+         return year;
+     }
+
+     public void setYear(int year) {
+         this.year = year;
+     }
+
+     public int getSpeed() {
+         return speed;
+     }
+
+     public void setSpeed(int speed) {
+         this.speed = speed;
+     }
+
+     public String getModel() {
+         return model;
+     }
+
+     public void setModel(String model) {
+         this.model = model;
+     }
+
+     public String getColor() {
+         return color;
+     }
+
+     public void setColor(String color) {
+         this.color = color;
+     }
+
+     public String getBrand() {
+         return brand;
+     }
+
+     public void setBrand(String brand) {
+         this.brand = brand;
+     }
+
+     public void accelerate(int increment){
+        speed+= increment;
+
+    }
+
+    public void brake(int decrement){
+        speed-= decrement;
+
+    }
+
+
 }
