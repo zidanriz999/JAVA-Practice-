657eabb6b3f614ac795315f594c17a45c3863c23
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-10-06 14:16:45 +0600

    yes

---
+++ src/Four_Piller/Animal.java
@@ -0,0 +1,15 @@
+package Four_Piller;
+
+ class Animal {
+   String name;
+   String age;
+   String breed;
+
+}
+
+ class cat extends Animal{
+
+
+}
+
+
---
+++ src/JAVA_OOP/Basic_Java.java
@@ -0,0 +1,49 @@
+package JAVA_OOP;
+import java.util.*;
+public class Basic_Java {
+    public static void main(String[] args) {
+     Scanner sc=  new Scanner(System.in);
+       // System.out.println("Enter the Sum of Number: ");
+        System.out.println("Enter the Multipication Table Number: ");
+        int n= sc.nextInt();
+
+        //int sum=0;
+        //for(int i=0;i<=n;i++){
+        for(int i=1;i<11;i++){
+            System.out.println(i*n);
+          //  sum = sum +i;
+        }
+//        System.out.println("Sum Number: "+sum);
+
+//        System.out.println("Enter the age: ");
+//        int age = sc.nextInt();
+//        if(age>18){
+//            System.out.println("Adult");
+//        }
+//        else{
+//            System.out.println("Not Adult");
+//        }
+//
+
+
+
+//         for(int i=0;i<=10;i++){
+//             System.out.println(i );
+//            // System.out.println(i + " ");
+//         }
+
+
+//        int i=0;
+//        while(i<11){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i=0;
+//        do{
+//            System.out.println(i);
+//         i++;
+//        }while(i<11);
+
+    }
+}
