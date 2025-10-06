57c1be054483909dda317fab5f4c3372aa65f4d0
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-10-06 14:17:22 +0600

    yes

---
+++ src/JAVA_OOP/Varargs.java
@@ -0,0 +1,30 @@
+package JAVA_OOP;
+
+public class Varargs {
+
+//    static int sum (int a,int b){
+//        return a+b;
+//    }
+//
+//    static int sum (int a,int b,int c){
+//        return a+b+c;
+//    }
+//    static int sum (int a,int b,int c,int d){
+//        return a+b+c+d;
+//    }
+
+   static int sum(int...arr){
+      int result=0;
+       for(int a:arr){
+           result+=a;
+       }
+       return result;
+   }
+
+
+    public static void main(String[] args) {
+        System.out.println("The sum of 4 and 5 is : "+sum(4,5) );
+        System.out.println("The sum of 4,5 and 9 is : "+sum(4,5,9) );
+        System.out.println("The sum of 4,5,9 and 19 is : "+sum(4,5,9,19) );
+    }
+}
